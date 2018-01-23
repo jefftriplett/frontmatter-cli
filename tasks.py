8fb3212ccbc08643866f67c157e6d09499376517
Jeff Triplett <jeff.triplett@gmail.com>
2018-01-22 21:40:36 -0600

    :pencil: Adds invoke helpers
    Signed-off-by: Jeff Triplett <jeff.triplett@gmail.com>

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import run, task
+
+
+@task
+def git_push(context):
+    run('git push origin --all')
+
+
+@task
+def pypi(context):
+    run('python setup.py sdist')
+    run('python setup.py bdist_wheel')
+
+
+@task
+def pypi_upload(context):
+    run('python setup.py sdist upload')
+    run('python setup.py bdist_wheel upload')
