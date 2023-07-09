8bb41c0be26e297673d7c9ed52c4765a269239bb
Jeff Triplett <jeff.triplett@gmail.com>
2023-07-09 09:08:38 -0500

    :bookmark: Bump version 2023.7.4 -> 2023.7.5

--- frontmatter_cli/cli.py
+++ frontmatter_cli/cli.py
@@ -8,7 +8,7 @@ from pydash import set_
 
 __author__ = "Jeff Triplett"
 __email__ = "jeff.triplett@gmail.com"
-__version__ = "2023.7.4"
+__version__ = "2023.7.5"
 
 
 def validate_extra_context(ctx, param, value):
